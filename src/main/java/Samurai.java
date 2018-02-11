392a745915a3380841e4c8f2b788ed157aeb72bd
Blanderbuss <fedorakb@gmail.com>
2018-02-11 15:48:52 +0200

    Initial commit

---
+++ src/main/java/Samurai.java
@@ -0,0 +1,36 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Samurai {
+    private String name;
+    private Weapon weapon;
+
+    public Samurai(){
+
+    }
+
+    public Samurai(String name, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    @Autowired
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void hello(){
+        System.out.println("Samurai " + name + " killed you with " + weapon);
+    }
+}
---
+++ src/main/java/Weapon.java
@@ -0,0 +1,19 @@
+public class Weapon {
+    private String name;
+
+    public Weapon() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
